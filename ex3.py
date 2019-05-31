3b4b1f7afd10ae3191f264d0c3550ebadee78018
DanMeisler <danmoshem@gmail.com>
2019-05-31 09:56:50 +0300

    some refactoring build z

--- ex3.py
+++ ex3.py
@@ -1,14 +1,16 @@
 import numpy as np
 import argparse
 
+BLANK_CHARACTER = ' '
 
-def assert_input(y, p, alphabet):
+
+def get_input(args):
+    y = np.load(args.net_output_path)
+    p = list(args.phonemes)
+    alphabet = list(args.alphabet) + [BLANK_CHARACTER]
     assert y.shape[0] >= len(p), "output matrix doesnt match phonemes to be classified"
-    assert y.shape[1] == len(alphabet) + 1, "output matrix doesnt match alphabet to be classified"
-
-
-def load_output_matrix(net_output_path):
-    return np.load(net_output_path)
+    assert y.shape[1] == len(alphabet), "output matrix doesnt match alphabet to be classified"
+    return y, p, alphabet
 
 
 def parse_args():
@@ -20,11 +22,10 @@ def parse_args():
 
 
 def main(args):
-    y = load_output_matrix(args.net_output_path)
-    p = args.phonemes
-    alphabet = args.alphabet
-    assert_input(y, p, alphabet)
+    y, p, alphabet = get_input(args)
+    z = [BLANK_CHARACTER] + list(' '.join(p)) + [BLANK_CHARACTER]
     print(y)
+    print(z)
 
 
 if __name__ == "__main__":
