b5d3f7727e558a1f437cd5bd717704140f2a6fb1
DanMeisler <danmoshem@gmail.com>
2019-05-31 09:32:45 +0300

    add assertion on input

--- ex3.py
+++ ex3.py
@@ -2,6 +2,10 @@ import numpy as np
 import argparse
 
 
+def assert_input(y, p, alphabet):
+    assert y.shape[0] == len(p), "output matrix doesnt match phonemes to be classified"
+
+
 def load_output_matrix(net_output_path):
     return np.load(net_output_path)
 
@@ -16,6 +20,9 @@ def parse_args():
 
 def main(args):
     y = load_output_matrix(args.net_output_path)
+    p = args.phonemes
+    alphabet = args.alphabet
+    assert_input(y, p, alphabet)
     print(y)
 
 
