* floating point precision * Fix critical bug - first time index is 0 not 1 * Refactored a bit

ex3.py
@@ -1,31 +1,31 @@
 import numpy as np
 import argparse
 
-BLANK_PHONEME = ' '
-
 
 class CTC(object):
+    BLANK_PHONEME = ' '
+
     def __init__(self, y, alphabet):
         assert y.shape[1] == len(alphabet) + 1, "output matrix doesnt match alphabet"
         self._y = y
-        self._alphabet = alphabet + [BLANK_PHONEME]
+        self._alphabet = alphabet + [CTC.BLANK_PHONEME]
         self._z = None
 
     def forward(self, p):
-        self._z = [BLANK_PHONEME] + list(' '.join(p)) + [BLANK_PHONEME]
+        self._z = [CTC.BLANK_PHONEME] + list(CTC.BLANK_PHONEME.join(p)) + [CTC.BLANK_PHONEME]
         return (self._get_alpha(len(self._z) - 1, self._y.shape[0] - 1) +
                 self._get_alpha(len(self._z) - 2, self._y.shape[0] - 1))
 
     def _get_alpha(self, s, t):
         if t == 0:
             if s < 2:
-                return self._get_y_probability(1, self._z[s])
+                return self._get_y_probability(0, self._z[s])
             return 0
 
         if s == 0:
             return self._get_alpha(s, t - 1) * self._get_y_probability(t, self._z[s])
 
-        if (s == 1) or (self._z[s] == BLANK_PHONEME) or (self._z[s] == self._z[s - 2]):
+        if (s == 1) or (self._z[s] == CTC.BLANK_PHONEME) or (self._z[s] == self._z[s - 2]):
             return (self._get_alpha(s - 1, t - 1) + self._get_alpha(s, t - 1)) * self._get_y_probability(t, self._z[s])
 
         return (self._get_alpha(s - 2, t - 1) + self._get_alpha(s - 1, t - 1) +
@@ -47,7 +47,7 @@ def parse_args():
 
 def main(args):
     ctc = CTC(np.load(args.net_output_path), list(args.alphabet))
-    print(ctc.forward(list(args.phonemes)))
+    print("%.2f" % ctc.forward(list(args.phonemes)))
 
 
 if __name__ == "__main__":